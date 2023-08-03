bc9282fa1826606f1cf0be6112b59c24ff3abde4
Kwang Myung Yu <sguys99@gmail.com>
2023-08-04 08:29:45 +0900

    [notebook] effective xgboost ch13(complete)

--- xgboost/effective_xgboost/xg_helpers.py
+++ xgboost/effective_xgboost/xg_helpers.py
@@ -440,3 +440,33 @@ def plot_3d_scatter(
         opacity=opacity,
     )
     return fig
+
+
+from typing import Any, Dict, Sequence
+
+
+def trial2df(trial: Sequence[Dict[str, Any]]) -> pd.DataFrame:
+    """
+    Convert a Trial object (sequence of trial dictionaries)
+    to a Pandas DataFrame.
+
+    Parameters
+    ----------
+    trial : List[Dict[str, Any]]
+        A list of trial dictionaries.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with columns for the loss, trial id, and
+        values from each trial dictionary.
+    """
+    vals = []
+    for t in trial:
+        result = t["result"]
+        misc = t["misc"]
+        val = {k: (v[0] if isinstance(v, list) else v) for k, v in misc["vals"].items()}
+        val["loss"] = result["loss"]
+        val["tid"] = t["tid"]
+        vals.append(val)
+    return pd.DataFrame(vals)
