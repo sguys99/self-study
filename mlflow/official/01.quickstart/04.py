29b4ccaa7cde56fddfa885e2a39d0c0fdf5282df
Kwang Myung Yu <sguys99@gmail.com>
2023-08-07 22:16:21 +0900

    [mlflow] Modify official ch04

--- mlflow/official/01.quickstart/04.py
+++ mlflow/official/01.quickstart/04.py
@@ -8,6 +8,6 @@ from sklearn.datasets import load_diabetes
 db = load_diabetes()
 X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
 
-model = mlflow.sklearn.load_model("runs:/6af2a8f18ced4970809460fa8b9d705b") # 확인요..
-predictions = model.prdict(X_test)
+model = mlflow.sklearn.load_model("runs:/6af2a8f18ced4970809460fa8b9d705b/model") # id 뒤에 model을 입력해야 가능..
+predictions = model.predict(X_test)
 print(predictions)
