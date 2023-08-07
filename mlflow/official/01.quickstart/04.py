dd5910e0f317bc17d91827a62489a882d72898ff
Kwang Myung Yu <sguys99@gmail.com>
2023-08-07 22:06:01 +0900

    [mlflow] official ch04

---
+++ mlflow/official/01.quickstart/04.py
@@ -0,0 +1,13 @@
+# 특정 training run을 로드하여 추론하기
+
+import mlflow
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+
+db = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
+
+model = mlflow.sklearn.load_model("runs:/6af2a8f18ced4970809460fa8b9d705b") # 확인요..
+predictions = model.prdict(X_test)
+print(predictions)
